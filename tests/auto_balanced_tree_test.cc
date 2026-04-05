bd214bd76ad06d942515b5ca281b0f1a09135eae
Kevin P. Barry <ta0kira@gmail.com>
2026-04-05 10:45:05 -0700

    Adds build, empty tests, and reorganizes.

---
+++ tests/auto_balanced_tree_test.cc
@@ -0,0 +1,5 @@
+#include "cat_dist/auto_balanced_tree.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace cat_dist;
---
+++ tests/categorical_distribution_test.cc
@@ -0,0 +1,5 @@
+#include "cat_dist/categorical_distribution.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace cat_dist;
